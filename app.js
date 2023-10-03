ca9ed4daa657016a6160818dc3ffca8472ab32b9
mtruonglee <mtruong12345678910@gmail.com>
2023-10-03 16:05:29 +0700

    lab 2

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const contactsRouter = require('./app/routes/contact.route');
 const ApiError = require('./app/api-error');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
 
 app.get("/", (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const app = require('./app.js');
 const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.utils.js');
 
 //start server
-const port = config.app.port;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect('mongodb://localhost:27017/contactbook');
+        console.log('Connected to the database!');
+
+        const port = config.app.port;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log('Cannot connect to the database!', error);
+        process.exit();
+    }
+}
+
+startServer();
